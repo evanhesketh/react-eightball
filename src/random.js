0d341c475ada03f6f817bdff064041d8c56bd5cb
Alyssa Denary <alyssa.denary@gmail.com>
2023-04-18 15:00:30 -0400

    add docstring update idx to num

--- src/random.js
+++ src/random.js
@@ -1,5 +1,12 @@
-function getRandomIdx (max) {
+/** getRandomNum
+ *
+ * @param {Number} max exclusive max for random number generator
+ *
+ * Returns random number between 0 and max number
+ */
+
+function getRandomNum (max) {
   return Math.floor(Math.random() * max);
 }
 
-export default getRandomIdx;
+export default getRandomNum;
