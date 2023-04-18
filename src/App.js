d3116c1b4fea1f2a21c69dfcd88e785efe8a8e28
Alyssa Denary <alyssa.denary@gmail.com>
2023-04-18 15:03:36 -0400

    add docstring

--- src/App.js
+++ src/App.js
@@ -2,10 +2,17 @@ import './App.css';
 import answers from './anwers';
 import Eightball from './Eightball';
 
+/** App top level component
+ *
+ * No props or state.
+ *
+ * App --> Eightball(props=answers)
+ */
+
 function App() {
   return (
     <div className="App">
-      <Eightball answers={answers}/>
+      <Eightball answers={answers} />
     </div>
   );
 }
