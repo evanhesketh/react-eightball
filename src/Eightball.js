d76520fa50813de6d6d32605765c443ebba2bfe3
Alyssa Denary <alyssa.denary@gmail.com>
2023-04-18 14:52:54 -0400

    add docstring

--- src/Eightball.js
+++ src/Eightball.js
@@ -2,6 +2,18 @@ import getRandomIdx from "./random";
 import { useState } from "react";
 import "./Eightball.css";
 
+/** Eightball showing message with a color
+ *
+ * Props:
+ * - answers: array of objects of [{color, msg}...]
+ *
+ * State:
+ * - color: "black"  (changes background color of eightball)
+ * - msg: "Think of a question"
+ *
+ * App --> Eightball
+ */
+
 function Eightball({ answers }) {
   const [color, setColor] = useState("black");
   const [msg, setMsg] = useState("Think of a question");
