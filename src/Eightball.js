fce48145df1c0f49515a9b8e698117305572eda5
Alyssa Denary <alyssa.denary@gmail.com>
2023-04-18 14:47:53 -0400

    import css and format

--- src/Eightball.js
+++ src/Eightball.js
@@ -1,22 +1,27 @@
 import getRandomIdx from "./random";
 import { useState } from "react";
+import "./Eightball.css";
 
-function Eightball ({answers}) {
+function Eightball({ answers }) {
   const [color, setColor] = useState("black");
   const [msg, setMsg] = useState("Think of a question");
 
   function handleClick(evt) {
     const randomIdx = getRandomIdx(answers.length);
-    const {msg, color} = answers[randomIdx];
+    const { msg, color } = answers[randomIdx];
     setColor(color);
     setMsg(msg);
   }
 
   return (
-    <div style={{backgroundColor: color}} onClick={handleClick}>
+    <div
+      className="Eightball"
+      style={{ backgroundColor: color }}
+      onClick={handleClick}
+    >
       <p>{msg}</p>
     </div>
-  )
+  );
 }
 
 export default Eightball;
